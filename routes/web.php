cdd68cefa657a3e169fc257a9321b491beaf3f4e
tugrulcan1 <tugrul-can@hotmail.com>
2023-06-09 12:50:19 +0300

    update

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route :: get('/merhaba',function(){
+return "Merhaba";
+});
+
+Route :: get('/api/v1/merhaba',function(){
+    return ['mesaj'=>'Merhaba'];
+    });
+
+    Route :: get('/urun/{isim}/{id?}',function($urunadi,$id=0){
+        return "Ürün Adı: $id $urunadi";
+        })->name('urun_detay');
+
+        Route::get('/kampanya', function () {
+            return redirect()->route('urun_detay', ['isim'=>'elma' , 'id'=>7]);
+        });
